ac2a2789fed55d759a3fb9e83ea72c247b71ad14
Kacper <k.zarnicki@gmail.com>
2020-05-28 18:39:28 +0200

    remove event

--- events/admin.py
+++ events/admin.py
@@ -1,8 +1,6 @@
 from django.contrib import admin
-from .models import Event, Ticket, TicketList
+from .models import Event
 
 # Register your models here.
 
 admin.site.register(Event)
-admin.site.register(Ticket)
-admin.site.register(TicketList)
--- events/urls.py
+++ events/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("register/", views.Register, name="Rejestracja"),
+    path("register/", views.register, name="Rejestracja"),
     path("register/succes", views.suc, name="Powodzenie"),
 
 ]
